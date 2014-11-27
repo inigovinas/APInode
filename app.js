bbae7402b13997582e2745738e74261635904751
inigovinas <inigovinas@hotmail.com>
2014-11-27 13:45:44 +0100

    otro

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ var request = require('request');
 // **********************************************
 
 
-app.get('/', function (req, res) {
+app.get('/ciudad', function (req, res) {
 
-	// Donostia. Zubiri-Manteo
-	var url = 'http://api.openweathermap.org/data/2.5/weather?lat=43.3276658&lon=-1.9711435';
 
+	//227452418 usuario de linkedin
+	//var url = 'https://api.linkedin.com/v1/people::(~,hks0NPUMZF):(first-name,connections)';
+	//var url = 'http://www.faroo.com/api?q=iphone&start=1&length=10&l=en&src=news&f=json';
+
+	//http://api.accuweather.com/developers/samples
+	var info = 'http://apidev.accuweather.com/locations/v1/search?q=donostia&apikey=meSocYcloNe';
 	var request = require('request');
-	request({url:url, json:"true"}, function (error, response, body) {
+	request({url:info, json:"true"}, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			console.log(body);
 			res.json(body);
@@ -20,11 +24,24 @@ app.get('/', function (req, res) {
 			res.json({error:"request error"});
 		}
 	});
+});
 
+app.get('/tiempo', function (req, res) {
+
+	var tiempo = 'http://apidev.accuweather.com/currentconditions/v1/2324048.json?language=en&apikey=meSocYcloNe';
+
+	var request = require('request');
+	request({url:tiempo, json:"true"}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+			res.json(body);
+		} else {
+			res.json({error:"request error"});
+		}
+	});
 });
 
 
-
 var server = app.listen(process.env.PORT || 3000, function(){
-    console.log('Listening in port %d', server.address().port);
+	console.log('Listening in port %d', server.address().port);
 });
